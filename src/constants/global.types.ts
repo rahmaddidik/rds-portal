b9f9c722cfdf290c3af2a3944f8786ea55529633
Rahmad Didik <rahmad.shoffyanto@dipostar.com>
2021-09-08 16:52:34 +0700

    add path email

--- src/constants/global.types.ts
+++ src/constants/global.types.ts
@@ -1,6 +1,7 @@
 export enum PATH {
   'LINKED_IN' = 'https://www.linkedin.com/in/rahmad-didik-shoffyanto-186996173',
   'GITHUB' = 'https://github.com/rahmaddidik',
+  'EMAIL' = 'mailto:rahmaddidikshoffyanto@gmail.com',
   'DRIBBBLE' = 'https://dribbble.com/rahmaddidik',
   'YOUTUBE' = 'https://www.youtube.com/c/cvrdsofficial',
 }
