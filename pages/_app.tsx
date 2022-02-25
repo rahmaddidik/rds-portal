15774cc9151a205af07e6e073688921464ec6d9d
rahmaddidik <rahmaddidikshoffyanto@gmail.com>
2022-02-25 11:23:23 +0700

    add ga4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { hotjar } from 'react-hotjar';
+import ReactGA from 'react-ga4';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Layout } from '../src/components';
@@ -8,6 +9,8 @@ import 'aos/src/sass/aos.scss';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     hotjar.initialize(2849027, 6);
+    ReactGA.initialize('G-TH4LW9Q0HB');
+    ReactGA.send('pageview');
   }, []);
 
   return (
