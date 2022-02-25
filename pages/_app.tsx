011f684506f81e2faffd3ed4a0695ffcacda5def
rahmaddidik <rahmaddidikshoffyanto@gmail.com>
2022-02-25 10:58:02 +0700

    add hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
+import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Layout } from '../src/components';
 import 'aos/src/sass/aos.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    hotjar.initialize(2849027, 6);
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
